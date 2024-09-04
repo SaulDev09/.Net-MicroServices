09f73a4a0bcee469988e61ccdeb2c8bf52ab40d9
Saul Chipana <Saul.Dev09@gmail.com>
2024-09-04 23:34:27 +0200

    23 EmailAPI, Added

---
+++ SC.Services.EmailAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SC.Services.EmailAPI.Models;
+
+namespace SC.Services.EmailAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmailLogger> EmailLoggers { get; set; }
+    }
+}
---
+++ SC.Services.EmailAPI/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using SC.Services.EmailAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region [Setting up EF - INIT]
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+#endregion
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+ApplyMigration(); // Added
+app.Run();
+
+#region [EF Migration]
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
+#endregion
