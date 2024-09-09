730662860d807ab48f612c0b211b8733d9b42158
Saul Chipana <Saul.Dev09@gmail.com>
2024-09-09 19:53:44 +0200

    32 OrderAPI, Controller Added

---
+++ SC.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SC.Services.OrderAPI.Data;
+using SC.Services.OrderAPI.Models.Dto;
+using SC.Services.OrderAPI.Service.IService;
+using SC.Services.OrderAPI.Utility;
+
+namespace SC.Services.OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+        private IProductService _productService;
+
+        public OrderAPIController(IMapper mapper, AppDbContext db, IProductService productService)
+        {
+            _mapper = mapper;
+            _db = db;
+            _productService = productService;
+            this._response = new ResponseDto();
+        }
+
+        [Authorize]
+        [HttpPost("CreateOrder")]
+        public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+                orderHeaderDto.OrderTime = DateTime.Now;
+                orderHeaderDto.Status = SD.Status_Pending;
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
+
+                //OrderHeader orderCreated = await _db.OrderHeaders.AddAsync(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+                OrderHeader orderCreated = _db.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+                await _db.SaveChangesAsync();
+
+                orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
+                _response.Result = orderHeaderDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message.ToString();
+            }
+
+            return _response;
+        }
+    }
+}
