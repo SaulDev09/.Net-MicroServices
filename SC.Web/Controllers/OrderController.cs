1ed6aaa4dab2fc7a8e67bb28306f7d5e068caded
Saul Chipana <Saul.Dev09@gmail.com>
2024-09-21 18:09:00 +0200

    44 Web, Index, OrderList Page Consumes OrderAPI

--- SC.Web/Controllers/OrderController.cs
+++ SC.Web/Controllers/OrderController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SC.Web.Models;
+using SC.Web.Service.IService;
+using SC.Web.Utility;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace SC.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
         public IActionResult OrderIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeaderDto> list;
+            string userId = "";
+
+            if (!User.IsInRole(SD.RoleAdmin))
+            {
+                userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+            ResponseDto response = _orderService.Get(userId).GetAwaiter().GetResult();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDto>();
+            }
+            return Json(new { data = list });
         }
     }
 }
---
+++ SC.Web/wwwroot/js/order.js
@@ -0,0 +1,31 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: "/order/getall" },
+        "columns": [
+            { data: 'orderHeaderId', "width": "5%" },
+            { data: 'email', "width": "25%" },
+            { data: 'name', "width": "20%" },
+            { data: 'phones', "width": "10%" },
+            { data: 'status', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'orderHeaderId',
+                "width": "10%",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/order/orderDetail?orderId=${data}" class="btn btn-primary mx-2">
+                        <i class="bi bi-pencil-square"></i>
+                    </a>
+                    </div>`
+                },
+            },
+
+        ]
+    });
+}
