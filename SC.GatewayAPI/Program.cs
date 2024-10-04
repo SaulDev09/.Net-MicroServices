28cd92e90657ba8752340b17fc206fe7e755f4bd
Saul Chipana <Saul.Dev09@gmail.com>
2024-10-04 14:59:39 +0200

    50 GatewayAPI, Ocelot Added + JWT Shared

--- SC.GatewayAPI/Program.cs
+++ SC.GatewayAPI/Program.cs
@@ -1,6 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using SC.GatewayAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddAppAuthentication();
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
 app.Run();
