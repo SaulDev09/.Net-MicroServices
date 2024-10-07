196b6728b3ea0b7422f29069de86c7aa2a46d3db
Saul Chipana <Saul.Dev09@gmail.com>
2024-10-07 22:36:09 +0200

    61 EmailAPI, Consumes message from RabbitMQ

--- SC.Services.EmailAPI/Program.cs
+++ SC.Services.EmailAPI/Program.cs
@@ -17,6 +17,12 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
 optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 builder.Services.AddSingleton(new EmailService(optionBuilder.Options));
+
+// RabbitMQ
+/**************************************************************/
+builder.Services.AddHostedService<RabbitMQAuthConsumer>();
+/**************************************************************/
+
 builder.Services.AddSingleton<IAzureServiceBusConsumer, AzureServiceBusConsumer>();
 #endregion
 
