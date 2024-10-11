675a6f32052b4cc0125aacdf211e58f36df72733
Saul Chipana <Saul.Dev09@gmail.com>
2024-10-11 05:23:55 +0200

    68 EmailAPI, Bug Fixed RabbitMQAuthConsumer

--- SC.Services.EmailAPI/Program.cs
+++ SC.Services.EmailAPI/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddSingleton(new EmailService(optionBuilder.Options));
 
 // RabbitMQ
 /**************************************************************/
-builder.Services.AddHostedService<RabbitMQOrderConsumer>();
+builder.Services.AddHostedService<RabbitMQAuthConsumer>();
 builder.Services.AddHostedService<RabbitMQCartConsumer>();
 builder.Services.AddHostedService<RabbitMQOrderConsumer>();
 /**************************************************************/
